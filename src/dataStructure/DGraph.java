5c0b65c50fba9a22bde29e7c82b8ea8e7e05cffa
darsam44 <darsam44@gmail.com>
2019-12-22 12:14:46 +0200

    work on remove node

--- src/dataStructure/DGraph.java
+++ src/dataStructure/DGraph.java
@@ -8,16 +8,18 @@ import java.util.Set;
 
 
 public class DGraph implements graph{
-	private int countEdge=0;
+	private int MC=0;
 	//this hash will represents the node
-	Hashtable<Integer, node_data> Nodes = new Hashtable<Integer, node_data>();
+	Hashtable<Integer, node_data> Nodes;
 
 	/*The hash represent an edges by Hashtable<node_data, Hashtable<Integer, edge_data>>   */
-	Hashtable<node_data, Hashtable<Integer, edge_data>>  Edge= new Hashtable<node_data, Hashtable<Integer, edge_data>>();
+	Hashtable<node_data, Hashtable<Integer, edge_data>>  Edge;
 
-//	public DGraph() {
-//		hash.put(NODE.getKey(), NODE);
-//	}
+	public DGraph() {
+		Hashtable<Integer, node_data> Nodes = new Hashtable<Integer, node_data>();
+		Hashtable<node_data, Hashtable<Integer, edge_data>>  Edge= new Hashtable<node_data, Hashtable<Integer, edge_data>>();
+		this.MC=0;
+	}
 
 	@Override
 	public node_data getNode(int key) {
@@ -38,6 +40,7 @@ public class DGraph implements graph{
 	public void addNode(node_data n) {
 		Nodes ver = (Nodes) n; 
 		Nodes.put(n.getKey(), ver);
+		MC++;
 	}
 
 	@Override
@@ -46,7 +49,7 @@ public class DGraph implements graph{
 		Hashtable<Integer, edge_data> table= new Hashtable<Integer, edge_data>();
 		table.put(dest, edge);
 		Edge.put(Nodes.get(src), table );
-		countEdge++;
+		MC++;
 	}
 
 	@Override
@@ -60,22 +63,20 @@ public class DGraph implements graph{
 	}
 	
 	
-	//not finished
+	//finished
 	@Override
 	public node_data removeNode(int key) {
 		/*we will run on the Hashtable of the edges and check for every edge if the source or the dest is 
 		   the node of the key that we get*/
-		if (Nodes.containsKey(key)) {
 		node_data to_remove=Nodes.get(key); //the node that we need to remove
+		Set <node_data> nodes = Edge.keySet();
+		for (node_data node_data : nodes) {
+			Edge.get(node_data).remove(key);
+			//count eadge --
+		}
 		Edge.remove(to_remove);
-		Nodes.remove(key);
-		}
-		for (Hashtable<Integer, edge_data> hash : Edge.values()) {
-			if (hash.containsKey(key)) {
-			hash.remove(key);
-			}
-		}
-		return null;
+		MC++;
+		return Nodes.remove(key);
 	}
 
 	//the function isn't right
@@ -84,7 +85,7 @@ public class DGraph implements graph{
 		node_data srcNode= Nodes.get(src); //get the node_data of src
 		this.countEdge--;
 		return Edge.get(srcNode).remove(dest); //remove the edge that start from src and ends in dest
-		
+		MC++;
 	}
 
 	@Override
--- src/dataStructure/Edges.java
+++ src/dataStructure/Edges.java
@@ -7,19 +7,19 @@ public class Edges implements edge_data {
 	private double WEIGHT;
 	private String INFO;
 	private int TAG;
+	private int countEdge;
 	
 	public Edges (node_data SRC, node_data DEST, double WEIGHT) {
 		this.SRC = SRC;
 		this.DEST = DEST;
 		this.WEIGHT =WEIGHT;
 	}
-	
-	
+		
 	@Override
 	public int getSrc() {
 		return this.SRC.getKey();
 	}
-
+	
 	@Override
 	public int getDest() {
 		return this.DEST.getKey();
@@ -49,5 +49,12 @@ public class Edges implements edge_data {
 	public void setTag(int t) {
 		this.TAG=t;
 	}
+	
+	public void setcountEdge(int countEdge) {
+		this.countEdge=countEdge;
+	}
 
+	public int getcountEdge () {
+		return this.countEdge;
+	}
 }
